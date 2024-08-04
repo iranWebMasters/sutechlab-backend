ca1449b1756ab095b745546b158b7c0977834fce
alireza <58270983+ansarialireza@users.noreply.github.com>
2024-08-04 18:24:43 +0330

    ✅ update services app

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import Category, Services
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Category, CategoryAdmin)
+
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = ('title', 'status', 'published_date', 'created_date', 'updated_date')
+    list_filter = ('status', 'category')
+    search_fields = ('title', 'content')
+    date_hierarchy = 'published_date'
+    readonly_fields = ('created_date', 'updated_date')
+    exclude = ('published_date',) 
+    filter_horizontal = ('category',)
+    
+    
+
+admin.site.register(Services, ServiceAdmin)
