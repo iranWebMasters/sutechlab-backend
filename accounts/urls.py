fde1cbcea856a1de7447e3b756bce528a964aca4
alireza <58270983+ansarialireza@users.noreply.github.com>
2024-08-16 11:43:34 +0330

    ✅ add register

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,4 +7,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('login/', LoginView.as_view(), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
 from django.contrib.auth.views import LogoutView,LoginView
+from django.contrib.auth.forms import UserCreationForm
+from django.http import HttpResponse
+from django.views.generic import FormView
 from django.contrib import messages
 from django.shortcuts import redirect
+from django.urls import reverse_lazy
 
 class LoginView(LoginView):
     def form_valid(self, form):
@@ -11,3 +15,14 @@ class LogoutView(LogoutView):
     def dispatch(self, request, *args, **kwargs):
         messages.success(request, "شما با موفقیت از سایت خارج شدید.")
         return super().dispatch(request, *args, **kwargs)
+
+class RegisterView(FormView):
+    template_name = 'registration/register.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accounts:login')
+    
+    def form_valid(self, form):
+        form.save()
+        messages.success(self.request,"ثبت نام با موفقیت انجام شد")
+        return super().form_valid(form)
+    
