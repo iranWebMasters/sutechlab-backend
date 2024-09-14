46226c3be2ec74b669c0a7622f35599f6f3c264e
alireza <58270983+ansarialireza@users.noreply.github.com>
2024-09-14 15:48:42 +0330

    ⚡️

--- services/serializers.py
+++ services/serializers.py
@@ -2,39 +2,38 @@ from rest_framework import serializers
 from .models import *
 
 class UnitAmountSerializer(serializers.ModelSerializer):
-    unit_display = serializers.SerializerMethodField()  # فیلد جدید برای نمایش واحد
+    unit_display = serializers.SerializerMethodField()
 
     class Meta:
-        model = Unit_amount  # فرض بر این است که این مدل را از کد شما داریم
-        fields = ['id', 'amount', 'unit', 'unit_display']  # اضافه کردن unit_display به fields
+        model = Unit_amount 
+        fields = ['id', 'amount', 'unit', 'unit_display'] 
 
     def get_unit_display(self, obj):
-        return obj.get_unit_display()  # استفاده از متد get_unit_display
+        return obj.get_unit_display()  
 
 class UnitPriceSerializer(serializers.ModelSerializer):
-    currency_display = serializers.SerializerMethodField()  # فیلد جدید برای نمایش واحد پول
-
+    currency_display = serializers.SerializerMethodField()  
     class Meta:
         model = Unit_price
-        fields = ['id', 'unit_price', 'currency', 'currency_display']  # اضافه کردن currency_display به fields
+        fields = ['id', 'unit_price', 'currency', 'currency_display'] 
 
     def get_currency_display(self, obj):
-        return obj.get_currency_display()  # استفاده از متد get_currency_display
+        return obj.get_currency_display()
 
 class ParametersSerializer(serializers.ModelSerializer):
     unit_amount = UnitAmountSerializer()
     unit_price = UnitPriceSerializer()
-    unit_display = serializers.SerializerMethodField()  # فیلد جدید برای نمایش واحد
+    unit_display = serializers.SerializerMethodField() 
 
     class Meta:
         model = Parameters
         fields = ['id', 'name', 'unit', 'unit_display', 'laboratory', 'unit_amount', 'unit_price']
 
     def get_unit_display(self, obj):
-        return obj.get_unit_display()  # استفاده از متد get_unit_display
+        return obj.get_unit_display()  
 
 class StandardsSerializer(serializers.ModelSerializer):
-    parameters = ParametersSerializer(many=True)  # استفاده از سریالایزر پارامترها
+    parameters = ParametersSerializer(many=True)  
 
     class Meta:
         model = Standards
@@ -46,7 +45,7 @@ class SampleSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class TestsSerializer(serializers.ModelSerializer):
-    standards = StandardsSerializer(many=True)  # استفاده از سریالایزر استانداردها
+    standards = StandardsSerializer(many=True) 
 
     class Meta:
         model = Tests
