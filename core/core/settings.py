b606c2757e9e903ae5e0156ba034b4bd02a6c518
alireza <58270983+ansarialireza@users.noreply.github.com>
2024-12-04 11:40:54 +0330

    up

--- core/core/settings.py
+++ core/core/settings.py
@@ -141,7 +141,7 @@ SECURE_HSTS_INCLUDE_SUBDOMAINS = True
 SECURE_HSTS_PRELOAD = True
 
 ## that requests over HTTP are redirected to HTTPS. aslo can config in webserver
-SECURE_SSL_REDIRECT = True
+SECURE_SSL_REDIRECT = False
 
 # for more security
 CSRF_COOKIE_SECURE = True
