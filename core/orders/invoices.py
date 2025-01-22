264284678e1aba829d62eda10b3431f79844c1a2
alireza <58270983+ansarialireza@users.noreply.github.com>
2025-01-22 17:28:04 +0330

    ✅

--- core/orders/invoices.py
+++ core/orders/invoices.py
@@ -1,123 +1,58 @@
-from django.conf import settings
-from django.db import models
-from accounts.models import User
-from services.models import Experiment, Test, Parameters
-from .utils import generate_order_code
-import json
+from django.template.loader import render_to_string
+from xhtml2pdf import pisa
+import jdatetime
 import os
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
-from reportlab.pdfbase.ttfonts import TTFont
-from reportlab.pdfbase import pdfmetrics
+from django.conf import settings
 import arabic_reshaper
 from bidi.algorithm import get_display
+from website.models import Contact
+from django.contrib.staticfiles import finders
 
-def reshape_text(text):
-    reshaped_text = arabic_reshaper.reshape(text)  # Connects letters
-    bidi_text = get_display(reshaped_text)  # Converts to right-to-left
-    return bidi_text
+def fetch_resources(uri, rel):
+    if uri.startswith('http://') or uri.startswith('https://'):
+        return uri
+    if uri.startswith(settings.STATIC_URL):
+        path = uri.replace(settings.STATIC_URL, '')
+        # Ensure that the full absolute path is returned
+        return os.path.join(settings.STATIC_ROOT, path)
+    return None
 
 def generate_invoice(order):
-    # Path for the invoice PDF file
+    # مسیر ذخیره فایل PDF
     pdf_directory = os.path.join(settings.MEDIA_ROOT, 'invoices')
-    
-    # Check if the directory exists, if not, create it
     if not os.path.exists(pdf_directory):
         os.makedirs(pdf_directory)
 
-    # Path for the invoice PDF file
     pdf_file_path = os.path.join(pdf_directory, f"invoice_{order.id}.pdf")
 
-    # Create PDF
-    c = canvas.Canvas(pdf_file_path, pagesize=letter)
-    width, height = letter
+    # داده‌های لازم برای قالب
+    creation_date = jdatetime.datetime.fromgregorian(date=order.created_at).strftime('%Y/%m/%d')
+    user = order.user
+    contact = Contact.objects.first()
 
-    # Register Persian font (ensure the path is correct)
-    pdfmetrics.registerFont(TTFont('Persian', 'statics/fonts/Vazir.ttf'))
+    # بارگذاری HTML از فایل
+    html_content = render_to_string('orders/invoice_template.html', {
+        'order': order,
+        'creation_date': creation_date,
+        'user': user,
+        'contact': contact,
+    })
 
-    # Set the font to Persian
-    c.setFont("Persian", 12)
+    # تابع تبدیل HTML به PDF
+    def convert_html_to_pdf(source_html, output_filename):
+        with open(output_filename, "w+b") as result_file:
+            pisa_status = pisa.CreatePDF(
+                source_html,
+                dest=result_file,
+                link_callback=fetch_resources
+            )
+        return pisa_status.err
 
-    # Title
-    c.drawRightString(width - 100, height - 50, reshape_text(f"پیش فاکتور برای درخواست ID: {order.id}"))
+    # ایجاد فایل PDF
+    convert_html_to_pdf(html_content, pdf_file_path)
 
-    # User Information
-    c.drawRightString(width - 100, height - 70, reshape_text(f"کاربر: {order.user.email}"))
-    c.drawRightString(width - 100, height - 90, reshape_text(f"کد مشتری: {order.user.customer_code}"))
-    c.drawRightString(width - 100, height - 110, reshape_text(f"نام و نام خانوادگی: {order.user.profile.first_name} {order.user.profile.last_name}"))
-    c.drawRightString(width - 100, height - 130, reshape_text(f"تاریخ ثبت درخواست: {order.created_at.strftime('%Y-%m-%d')}"))
-    c.drawRightString(width - 100, height - 150, reshape_text(f"وضعیت: {order.get_status_display()}"))
+    # ذخیره مسیر PDF در مدل سفارش
+    order.invoice_pdf = f'invoices/invoice_{order.id}.pdf'
+    order.save()
 
-    y_position = height - 170  # Update y_position after user info
-    c.line(50, y_position, width - 50, y_position)  # Draw line separator
-    y_position -= 10  # Move down after the line
-
-    # Experiment Information
-    c.drawRightString(width - 100, y_position, reshape_text(f"آزمایش: {order.experiment.test_name if order.experiment else 'N/A'}"))
-    y_position -= 20
-
-    # Sample Information
-    c.drawRightString(width - 100, y_position, reshape_text("اطلاعات نمونه‌ها:"))
-    y_position -= 20
-
-    for sample in order.sample_info.all():
-        c.drawRightString(width - 120, y_position, reshape_text(f"نوع نمونه: {sample.sample_type}"))
-        y_position -= 20
-        c.drawRightString(width - 120, y_position, reshape_text(f"نام نمونه مشتری: {sample.customer_sample_name}"))
-        y_position -= 20
-        c.drawRightString(width - 120, y_position, reshape_text(f"تعداد: {sample.sample_count}"))
-        y_position -= 20
-        c.drawRightString(width - 120, y_position, reshape_text(f"شرایط نگهداری: {sample.storage_conditions}"))
-        y_position -= 20
-        c.drawRightString(width - 120, y_position, reshape_text(f"توضیحات نمونه: {sample.sample_description}"))
-        y_position -= 20
-        if sample.file_upload:
-            c.drawRightString(width - 120, y_position, reshape_text(f"فایل تکمیلی نمونه: {sample.file_upload.url}"))
-            y_position -= 20
-
-    y_position -= 10  # Move down after the last sample info line
-    c.line(50, y_position, width - 50, y_position)  # Draw line separator
-    y_position -= 10  # Move down after the line
-
-    # Test Information
-    c.drawRightString(width - 100, y_position, reshape_text("اطلاعات آزمایش‌ها:"))
-    y_position -= 20
-
-    for test in order.test_info.all():
-        c.drawRightString(width - 120, y_position, reshape_text(f"شناسه نمونه آزمایش: {test.user_sample.customer_sample_name}"))
-        y_position -= 20
-        c.drawRightString(width - 120, y_position, reshape_text(f"عنوان آزمایش: {test.test.name_fa if test.test else 'N/A'}"))
-        y_position -= 20
-        c.drawRightString(width - 120, y_position, reshape_text(f"تعداد تکرار آزمون: {test.repeat_count_test}"))
-        y_position -= 20
-        c.drawRightString(width - 120, y_position, reshape_text(f"پارامتر: {test.parameter}"))
-        y_position -= 20
-        c.drawRightString(width - 120, y_position, reshape_text(f"مقدار پارامتر: {test.get_parameter_values_dict()}"))  # Assuming this returns a dict
-        y_position -= 20
-
-    y_position -= 10  # Move down after the last test info line
-    c.line(50, y_position, width - 50, y_position)  # Draw line separator
-    y_position -= 10  # Move down after the line
-
-    # Discount Information
-    discount_info = order.discount_info  # Assuming one discount info per order
-    c.drawRightString(width - 100, y_position, reshape_text("اطلاعات تخفیف:"))
-    y_position -= 20
-    if discount_info:
-        c.drawRightString(width - 120, y_position, reshape_text(f"عضو هیات علمی: {'بله' if discount_info.is_faculty_member else 'خیر'}"))
-        y_position -= 20
-        c.drawRightString(width - 120, y_position, reshape_text(f"دانشجو یا کارکنان دانشگاه: {'بله' if discount_info.is_student_or_staff else 'خیر'}"))
-        y_position -= 20
-        c.drawRightString(width - 120, y_position, reshape_text(f"متقاضی تخفیف نهادهای طرف قرارداد: {'بله' if discount_info.is_affiliated_with_institution else 'خیر'}"))
-        y_position -= 20
-        if discount_info.contract_party_file:
-            c.drawRightString(width - 120, y_position, reshape_text(f"نام نهاد تخفیف: {discount_info.contract_party_file.url}"))
-
-    # Finalizing PDF
-    c.save()
-
-    # Set the path in the order without 'media/'
-    order.invoice_pdf = f'invoices/invoice_{order.id}.pdf'  # Only save the relative path
-    order.save()  # Save the order to update the invoice_pdf field
-
-    return pdf_file_path  # Return the path to the PDF file
+    return pdf_file_path
--- core/orders/signals.py
+++ core/orders/signals.py
@@ -10,3 +10,13 @@ def create_invoice(sender, instance, created, **kwargs):
         pdf_file_path = generate_invoice(instance)
         instance.invoice_pdf = pdf_file_path
         instance.save()
+
+@receiver(post_save, sender=Order)
+def update_invoice(sender, instance, created, **kwargs):
+    # فقط در صورتی که سفارش به‌روزرسانی شده باشد و کامل باشد، پیش‌فاکتور را به‌روزرسانی کنید
+    if not created and instance.is_complete:
+        # بررسی اینکه آیا پیش‌فاکتور قبلاً تولید شده است
+        if not instance.invoice_pdf:  # اگر پیش‌فاکتور وجود ندارد
+            pdf_file_path = generate_invoice(instance)
+            instance.invoice_pdf = pdf_file_path
+            instance.save(update_fields=['invoice_pdf'])  # فقط فیلد پیش‌فاکتور را به‌روزرسانی کنید
