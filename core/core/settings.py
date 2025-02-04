95b9d8ae636c4ccfebc9977e3b96c137b4de0814
alireza <58270983+ansarialireza@users.noreply.github.com>
2025-02-04 13:04:40 +0330

    ✅

--- core/core/settings.py
+++ core/core/settings.py
@@ -79,11 +79,11 @@ WSGI_APPLICATION = 'core.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': os.environ.get('DATABASE_NAME',),
-        'USER': os.environ.get('DATABASE_USER',),
-        'PASSWORD': os.environ.get('DATABASE_PASSWORD',),
-        'HOST': os.environ.get('DATABASE_HOST',),
-        'PORT': os.environ.get('DATABASE_PORT',),
+        'NAME': os.getenv('DATABASE_NAME'),
+        'USER': os.getenv('DATABASE_USER'),
+        'PASSWORD': os.getenv('DATABASE_PASSWORD'),
+        'HOST': os.getenv('DATABASE_HOST'),
+        'PORT': os.getenv('DATABASE_PORT'),
     }
 }
 # DATABASES = {
