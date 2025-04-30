72c1a082fcba18bb437966f523753f49c84ab311
alireza <58270983+ansarialireza@users.noreply.github.com>
2025-04-30 16:50:09 +0330

    ✅

--- core/core/settings.py
+++ core/core/settings.py
@@ -162,9 +162,6 @@ SESSION_COOKIE_AGE = config(
     "SESSION_COOKIE_AGE", default=1209600, cast=int
 )  # 2 weeks in seconds
 
-# CSRF Trusted Origins
-CSRF_TRUSTED_ORIGINS = config("CSRF_TRUSTED_ORIGINS", default="").split(",")
-
 # Registration URL Redirects
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/"
